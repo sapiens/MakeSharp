a4d164cf0471086c30f7eceda3521196f15d162c
RejectKid <RejectKid@gmail.com>
2016-01-09 19:58:20 -0500

    added .Xunit() extension method to run tests and return bool on pass/fail

---
+++ src/MakeSharp/XunitExtensionMethods.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Threading;
+using Xunit.Runners;
+
+namespace MakeSharp
+{
+    public static class XunitExtensionMethods
+    {
+        static readonly ManualResetEvent Finished = new ManualResetEvent(false);
+        private static bool _result = true;
+
+        public static bool XUnit(this string dllName)
+        {
+            if (!File.Exists(dllName)) return false;
+
+            using (var runner = AssemblyRunner.WithAppDomain(dllName))
+            {
+                runner.OnTestSkipped += info => { _result = false; };
+                runner.OnTestFailed += info => { _result = false; };
+                runner.OnExecutionComplete += info => { Finished?.Set(); };
+                runner.Start();
+
+                Finished.WaitOne();
+                Finished.Dispose();
+            }
+            return _result;
+        }
+    }
+}
---
+++ src/Tests/XUnitExtensionTests.cs
@@ -0,0 +1,22 @@
+﻿using MakeSharp;
+using Xunit;
+
+namespace Tests
+{
+    public class XUnitExtensionTests
+    {
+        [Fact]
+        public void Run_Xunit_Extension_False()
+        {
+            var results = "something.dll".XUnit();
+            Assert.False(results);
+        }
+
+        [Fact]
+        public void Run_Xunit_Extension_True()
+        {
+            var results = @"..\..\Resources\Tests.dll".XUnit();
+            Assert.True(results);
+        }
+    }
+}
